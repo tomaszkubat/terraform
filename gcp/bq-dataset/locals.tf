cb7d04a14e2ff14f83ccc0b09526c85d032127d2
Tomasz Kubat <tomasz.kubat@wp.pl>
2025-05-26 10:53:49 +0200

    tf format

--- gcp/bq-dataset/locals.tf
+++ gcp/bq-dataset/locals.tf
@@ -1,10 +1,10 @@
 locals {
-  meta_subpath = "meta/"
+  meta_subpath   = "meta/"
   schema_subpath = "schema/"
 
   table_meta_path   = "tables/${local.meta_subpath}"
   table_schema_path = "tables/${local.schema_subpath}"
-  
-  view_meta_path    = "views/${local.meta_subpath}"
-  view_schema_path  = "views/${local.schema_subpath}"
+
+  view_meta_path   = "views/${local.meta_subpath}"
+  view_schema_path = "views/${local.schema_subpath}"
 }
