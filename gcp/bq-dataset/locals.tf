1017e810d94431be0b46321531813f030efa6afe
Tomasz Kubat <tomasz.kubat@wp.pl>
2025-05-28 12:58:23 +0200

    terraform fmt

--- gcp/bq-dataset/locals.tf
+++ gcp/bq-dataset/locals.tf
@@ -1,8 +1,8 @@
 locals {
   # resource paths - concatenate the relative configuration path with resource subpaths
   dataset_path = "${var.configuration_path}/dataset/"
-  tables_path = "${var.configuration_path}/tables/"
-  views_path = "${var.configuration_path}/views/"
+  tables_path  = "${var.configuration_path}/tables/"
+  views_path   = "${var.configuration_path}/views/"
   # subpaths
   meta_subpath   = "meta/"
   schema_subpath = "schema/"
--- gcp/bq-dataset/view.tf
+++ gcp/bq-dataset/view.tf
@@ -18,7 +18,7 @@ locals {
 
 resource "google_bigquery_table" "view" {
   # we expect to create tables before
-  depends_on = [ google_bigquery_table.table ]
+  depends_on = [google_bigquery_table.table]
 
   for_each = local.view_config_map
 
