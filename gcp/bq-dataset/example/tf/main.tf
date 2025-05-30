c77b99ab1db603de220cdd94d2ce157cab44852a
Tomasz Kubat <tomasz.kubat@wp.pl>
2025-05-30 12:47:27 +0200

    added local path in example

--- gcp/bq-dataset/example/tf/main.tf
+++ gcp/bq-dataset/example/tf/main.tf
@@ -1,12 +1,15 @@
 
 module "bq_dataset_example_usage" {
   # clone over https example
+  # for remote usage use:
   source = "github.com/tomaszkubat/terraform/gcp/bq-dataset"
+  # for local usage use:
+  # source = "./../../"
 
   # path to configuration files
   configuration_path = "./../config/my_dataset"
 
   # input parameters
-  project_id = "<YOUR_PROJECT_ID"
+  project_id = "<MY_PROJECT_ID>"
   dataset_prefix = "test_"
 }
