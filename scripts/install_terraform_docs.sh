da89cb613105587ca0c004b5339e2da9d153a3ed
Tomasz Kubat <tomasz.kubat@wp.pl>
2025-06-03 12:47:58 +0200

    added terraform-docs installation script

---
+++ scripts/install_terraform_docs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://terraform-docs.io/user-guide/installation/
+curl -sSLo ./terraform-docs.tar.gz https://terraform-docs.io/dl/v0.20.0/terraform-docs-v0.20.0-$(uname)-amd64.tar.gz
+tar -xzf terraform-docs.tar.gz
+chmod +x terraform-docs
+mv terraform-docs /usr/local/bin/terraform-docs
